ab6f187a64e1035bdcaa346985c2f4f5751eb094
Federico Bartsch <fob2002ar@yahoo.com>
2022-06-29 23:19:12 -0300

    Primer desafio JS

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ for (var contador = 0; contador < 3 && acierto === false; contador++) {
     var respUsuario = prompt("Capital de Argentina: ");
         if (respUsuario.toUpperCase() === respuesta) { 	 	 
         var acierto = true; 
-        alert("Enhorabuena, has acertado");	 	
+        alert("Respuesta correcta, has acertado!!!");	 	
 	    }	 	 
 }	 	 
 if (!acierto) {alert("Lo sentimos, has consumido tus 3 intentos")}; 
